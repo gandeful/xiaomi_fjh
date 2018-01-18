9ba4091a96740dbba6f542782ba434a95ad0f94d
gandeful <863529565@qq.com>
2018-01-18 23:32:36 +0800

    header完成

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('.shopcart').mouseover(function(){
+    $('.incart').stop().slideDown();
+}).mouseout(function(){
+    $('.incart').stop().slideUp();
+})
+
+$('.incart').mouseover(function(){
+    $('.incart').stop().slideDown();
+}).mouseout(function(){
+    $('.incart').stop().slideUp();
+
+})
